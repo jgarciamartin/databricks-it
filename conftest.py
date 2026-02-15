565c15ce74987102c551efa1ae6f98ff7b25a9a4
jgarciama14 <j.garciamartin92@gmail.com>
2026-02-15 12:23:44 +0100

    Actualizacion

--- conftest.py
+++ conftest.py
@@ -1,8 +1,11 @@
 import pytest
+import os
 
 @pytest.fixture(scope="session")
 def spark():
-    from databricks.connect import DatabricksSession
+    print("DATABRICKS_HOST:", os.getenv("DATABRICKS_HOST"))
+    print("DATABRICKS_SERVERLESS_COMPUTE_ID:", os.getenv("DATABRICKS_SERVERLESS_COMPUTE_ID"))
+    from databricks.connect import DatabricksSession    
     s = DatabricksSession.builder.getOrCreate()
     yield s
     s.stop()
